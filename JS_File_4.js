5f8f7f0d4eb6ad39032d1d9d29a389949a83564f
Waseem Kasmani <154504249+waseem-aziz-kasmani@users.noreply.github.com>
2025-07-03 15:02:44 +0500

    Update JS_File_4.js

--- JS_File_4.js
+++ JS_File_4.js
@@ -2,12 +2,12 @@ function login_1(){
 let y = document.getElementById("PW1").value;
 let x = document.getElementById("PW2");
 const z = localStorage.getItem("Var1");
-alert("PSS WD : " + z); 
+
 if (x.checked == true){
 
    localStorage.setItem("Var1",y);
    
 } else {
-  alert("else");
+
 }
 }
